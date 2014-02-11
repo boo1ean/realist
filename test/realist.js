4116b54aa4887b0547a17b15a295c17a3f2e715b
boo1ean <boo1ean0807@gmail.com>
2014-02-12 01:25:04 +0200

    Add test for resolve options

---
+++ test/realist.js
@@ -0,0 +1,60 @@
+var Realist = require('../');  
+
+describe('Realist', function() {
+	var options = {
+		path: {
+			keys: ['p', 'path'],
+			description: 'Path to migrations base directory'
+		},
+
+		force: {
+			keys: ['f', 'force'],
+			description: 'Force down all migrations'
+		},
+
+		nosave: {
+			keys: ['n', 'nosave'],
+			description: 'Do not save migrations data to storage'
+		},
+
+		storage: {
+			keys: ['s', 'storage'],
+			description: 'Use specific migrations storage'
+		},
+
+		name: {
+			keys: ['good-enough-option']
+		}
+	};
+
+	var actions = {
+		'up [steps]': function() {},
+		'down [steps]': function() {},
+		'refresh': function() {},
+		'create <name>': function() {}
+	};
+
+	var realist = new Realist(options, actions);
+
+	var givenOptions = {
+		'p': 'much path',
+		'nosave': true,
+		'f': 'good',
+		'storage': 'best storage',
+		'good-enough-option': 'Johny'
+	};
+
+	var expectedOptions = {
+		'path': 'much path',
+		'nosave': true,
+		'force': 'good',
+		'storage': 'best storage',
+		'name': 'Johny'
+	};
+
+	describe('resolveOptions', function() {
+		it('Should resolve options according to option map', function() {
+			realist.resolveOptions(givenOptions).should.be.eql(expectedOptions);
+		});
+	});
+});
